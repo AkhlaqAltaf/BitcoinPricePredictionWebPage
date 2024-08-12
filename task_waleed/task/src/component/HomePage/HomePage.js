19a58d9f0b0471ff7c683afbd668cf6ab0fd9ae4
Ahsan Zeb <130226758+ahsan-zeb10@users.noreply.github.com>
2024-08-13 03:41:13 +0500

    task complete

---
+++ task_waleed/task/src/component/HomePage/HomePage.js
@@ -0,0 +1,21 @@
+import "./HomePage.css";
+
+// import Navbar from "../navbar/Navbar";
+function HomePage() {
+  return (
+      <div className="container">
+        <div className="box">
+          <h1>Prediction Of Bitcoin</h1>
+
+          <h4>
+            Bitcoin is the public blockchain used to create and manage the
+            cryptocurrency of the same name. Bitcoin mining is the race between
+            miners to hash specific values and other block information to find
+            the solution to a hashing problem and add a block to the blockchain.
+            The winning miner is rewarded with bitcoins.
+          </h4>
+        </div>
+      </div>
+  );
+}
+export default HomePage;
